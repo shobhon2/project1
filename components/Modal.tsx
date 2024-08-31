f2fae27b920c60c6e6a1f4ce9b92bb002f598132
Shobhon Das <shobhondas2002@gmail.com>
2024-08-31 14:14:30 +0530

    deleted the transition child animation thing

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -38,32 +38,14 @@ const Modal = ({ productId }: Props) => {
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" onClose={closeModal} className="dialog-container">
           <div className="min-h-screen px-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0" /> 
-            </Transition.Child>
-
+            
+            
             <span
               className="inline-block h-screen align-middle"
               aria-hidden="true"
             />
             
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
+            
               <div className="dialog-content">
                 <div className="flex flex-col">
                   <div className="flex justify-between">
@@ -125,7 +107,6 @@ const Modal = ({ productId }: Props) => {
                   </button>
                 </form>
               </div>
-            </Transition.Child>
           </div>
         </Dialog>
       </Transition>
